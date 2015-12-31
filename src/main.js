70503d3469ececb4db6f0accc3d0f97edbb7c895
milan-hwj <wenjun.hwj@alibaba-inc.com>
2015-12-31 17:54:40 +0800

    clipboard callback

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@
 class ClipBoard{
     constructor() {}
     createContentNode(){
-    	/**
-    	 * @describe Create dom, For storing data to be copied
-    	 * @param    
-    	 * @return   dom
-    	 */
-    	var copyDom = document.createElement('pre');
+        /**
+         * @describe Create dom, For storing data to be copied
+         * @param
+         * @return   dom
+         */
+        var copyDom = document.createElement('pre');
         copyDom.style.position = 'absolute';
         copyDom.style.left = '-9999px';
         copyDom.style.top = '-9999px';
@@ -19,37 +19,91 @@ class ClipBoard{
         document.body.appendChild(copyDom);
         return copyDom;
     }
-    copy(content){
-    	/**
-    	 * @describe copy 
-    	 * @param    content: Data for replication
-    	 * @return   
-    	 */
-    	let copyDom = this.createContentNode();
-    	copyDom.innerHTML = content;
-    	let selection = window.getSelection();
-		let range = document.createRange();
+    copy(content, opt){
+        /**
+         * @describe copy
+         * @param    content: Data for replication
+         *           opt: {
+         *              success: success callback
+         *              error: error callback
+         *           }
+         * @return
+         */
+        opt = opt || {};
+        try {
+            // create
+            let copyDom = this.createContentNode();
+            copyDom.innerHTML = content;
+            let selection = window.getSelection();
+            let range = document.createRange();
 
-		range.selectNodeContents(copyDom);
-		selection.removeAllRanges();
-		selection.addRange(range);
-		document.execCommand('copy');
+            // select
+            range.selectNodeContents(copyDom);
+            selection.removeAllRanges();
+            selection.addRange(range);
+            document.execCommand('copy');
 
-		// destroy
-		this.destroy(copyDom);
+            // destroy
+            this.destroy(copyDom);
+            // callback
+            this.copyHandle(opt.success);
+        }
+        catch (err) {
+            this.copyHandle(opt.error);
+        }
+    }
+    copyHandle(callback){
+        if(!callback){
+            return;
+        }
+        callback.call();
     }
     destroy(dom){
-    	/**
-    	 * @describe The node is removed when copy complete 
-    	 * @param    dom
-    	 * @return   
-    	 */
-    	document.body.removeChild(dom);
+        /**
+         * @describe The node is removed when copy complete 
+         * @param    dom
+         * @return   
+         */
+        document.body.removeChild(dom);
     }
-    bind(opt){
-    	/*opt.trigger.addEventListener("click", function(){
-			copt(content);
-    	}*/
-	}
+    bind(doms, opt){
+       /**
+         * @describe Bind copy event, bind
+         * @param    doms: dom or [dom1, dom2, ...]
+         * @return
+         */
+        let self = this,
+            bindHandle = function(dom){
+                let copyHandle = function(){
+                    let attr = this.attributes['data-copy-content'] || {};
+                    self.copy(attr.value, opt);
+                };
+                if (dom.addEventListener){
+                    dom.addEventListener('click', function(){
+                        copyHandle.call(this);
+                    });
+                }
+                else if (dom.attachEvent) {
+                    dom.attachEvent( "onClick", function() {
+                        copyHandle.call(this);
+                    });
+                }
+            };
+        if(!doms){
+            return;
+        }
+        // doms
+        if(doms.length > 0){
+            for(var i=0; i<doms.length; i++){
+                if(doms[i] instanceof HTMLElement){
+                    bindHandle(doms[i]);
+                }
+            }
+        }
+        // dom
+        else{
+            bindHandle(doms);
+        }
+    }
 }
 window.ClipBoard = new ClipBoard();
